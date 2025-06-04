e479b6498c40793bb77414411ebfa11207cafdd9
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-06-04 16:21:24 +0200

    Complete snacks

---
+++ script.js
@@ -0,0 +1,79 @@
+/*1° Snack
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
+
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // ?  Double Cheese Burger
+console.log(secondBurger.name); // ? Double Cheese Burger 
+
+//RISPOSTA: oggetti creati in memoria: 1
+
+/*2° Snack
+P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?  "Salad"
+console.log(secondBurger.ingredients[0]); // ? "Salad"
+
+//RISPOSTA: oggetti creati in memoria: 3 (hamburger con relativo array di ingredienti, 2°hamburger ma non crea un nuovo array, è solo lo stesso array referenziato perché lo spread non copia gli oggetti annidati)
+
+/*3° Snack
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? */
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+//RISPOSTA: oggetti creati in memoria: 9 (hamburger, secondBurger, thirdBurger, ognuno con i relativi maker e restarant, quindi 3 burger, 3 weight e 3 maker)
+
+/*4° Snack
+Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?*/
+
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+}
+
+const restaurant = {
+    name: "Hyur's Burgers",
+    address: {
+        street: 'Main Street',
+        number: 123,
+    },
+    openingDate: new Date(2025, 3, 11),
+    isOpen: false,
+};
+
+/*RISPOSTA: 1. Spread, perché clona anche le funzioni 
+            2. structuredClone, perché clona anche gli oggetti complessi come new Date()*/
